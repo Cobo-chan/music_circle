9fa5f08e0661cec6127d5f795baead5b2d4f7090
Cobo-chan <ota2dbxc5up@gmail.com>
2021-03-06 16:30:34 +0900

    ユーザー登録用のバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :image
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname, :image
+  end
 end
